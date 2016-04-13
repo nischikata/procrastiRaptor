dea4d3747a6adfd8a0319b90a82b8a0304d69867
nischikata <nischikata@gmail.com>
2016-04-13 13:37:16 +0200

    dummy pages added

---
+++ add_task.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>ProcrastiRaptor - Welcome !</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+</head>
+<body>
+<nav>
+    <ul>
+        <a href="index.html"><li id="home"></li></a>
+        <a href="tasklist.html"><li id="tasklist"  class="active"></li></a>
+        <a href="calendar.php"><li id="calendar"></li></a>
+        <a href="stats.php"><li id="stats"></li></a>
+        <a href="user.php"><li style="float:right" id="user"></li></a>
+    </ul>
+</nav>
+
+<header> add task
+</header>
+
+
+
+</body>
+</html>
