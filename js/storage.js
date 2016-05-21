27fda43ed03abd2b2b2ede80880927c13bc5fb38
nischikata <nischikata@gmail.com>
2016-05-21 15:39:57 +0200

    indexeddb - edit entry implemented

--- js/storage.js
+++ js/storage.js
@@ -6,23 +6,13 @@ const DB_STORE_NAME = 'tasks';
 
 var db;
 
-// this is what the data looks like
-const taskList = [
-    { title: "Do 50 pushups.", notes: "none",
-      p_difficulty:  4, p_satisfaction:  3, p_time_effort: 40, priority: 'C', ranking: 5,
-      a_difficulty: -1, a_satisfaction: -1, a_time_effort: -1, done: false },
-    { title: "Find b-day present for best friend", notes: "T-Shirt?",
-        p_difficulty:  5, p_satisfaction:  4, p_time_effort: 90, priority: 'A', ranking: 1,
-        a_difficulty: -1, a_satisfaction: -1, a_time_effort: -1, done: false },
-];
-
-
 function openDb() {
     console.log("openDb ...");
     var req = indexedDB.open(DB_NAME, DB_VERSION);
     req.onsuccess = function (evt) {
         db = this.result;
         console.log("openDb DONE");
+        updateTaskListView();
 
     };
     req.onerror = function (evt) {
@@ -87,7 +77,7 @@ function addTask(title, notes, category, duedate, difficulty, satisfaction, time
         duration_field.css('background-color', 'white');
 
         displayActionSuccess("New Task \'" + title + "\' added successfully.");
-        displayTaskList();
+        updateTaskListView();
     }
 
     req.onerror = function() {
@@ -121,11 +111,87 @@ function clearObjectStore(store_name) {
     };
 }
 
-function updateTask(){}
+/**
+ * @param {number} key
+ * @param {IDBObjectStore=} store
+ * @param {string} name
+ * @param value
+ */
+function updateTask(key, name, value, store){
+    console.log("update Task: ", arguments);
+    if (typeof store == 'undefined') {
+        store = getObjectStore(DB_STORE_NAME, 'readwrite');
+    }
 
-function removeTask(){}
+    var request = store.get(key);
+    request.onerror = function(event) {
+        // Handle errors!
+    };
+    request.onsuccess = function(event) {
+        // Get the old value that we want to update
+        var data = request.result;
+
+        // update the value(s) in the object that you want to change
+        console.log("data name: " + data[name]);
+        data[name] = value;
+
+        // Put this updated object back into the database.
+        var requestUpdate = store.put(data);
+        requestUpdate.onerror = function(event) {
+            // Do something with the error
+        };
+        requestUpdate.onsuccess = function(event) {
+            // Success - the data is updated!
+            displayActionSuccess("Task updated successfully.");
+            // TODO: only update this one list element!
+            updateTaskListView();
+        };
+    };
+}
 
 
+/**
+ * @param {number} key
+ * @param {IDBObjectStore=} store
+ */
+function removeTask(key, store) {
+    console.log("delete Task:", arguments);
+
+    if (typeof store == 'undefined')
+        store = getObjectStore(DB_STORE_NAME, 'readwrite');
+
+    // As per spec http://www.w3.org/TR/IndexedDB/#object-store-deletion-operation
+    // the result of the Object Store Deletion Operation algorithm is
+    // undefined, so it's not possible to know if some records were actually
+    // deleted by looking at the request result.
+    var req = store.get(key);
+    req.onsuccess = function(evt) {
+        var task = evt.target.result;
+        console.log("Task:", task);
+        if (typeof task == 'undefined') {
+            displayActionFailure("No matching record found");
+            return;
+        }
+        // Warning: The exact same key used for creation needs to be passed for
+        // the deletion. If the key was a Number for creation, then it needs to
+        // be a Number for deletion.
+        req = store.delete(key);
+        req.onsuccess = function(evt) {
+            console.log("evt:", evt);
+            console.log("evt.target:", evt.target);
+            console.log("evt.target.result:", evt.target.result);
+            console.log("delete successful");
+            displayActionSuccess("Deletion successful");
+            updateTaskListView(store);
+        };
+        req.onerror = function (evt) {
+            console.error("delete task:", evt.target.errorCode);
+        };
+    };
+    req.onerror = function (evt) {
+        console.error("delete task:", evt.target.errorCode);
+    };
+}
 
 function addEventListeners(){
 
@@ -167,7 +233,59 @@ function addEventListeners(){
 
 }
 
-function displayTaskList(store) {
+function getTaskListElement(value, key) {
+    console.log("task content: ", arguments);
+    var list_item = $('<li class="task"></li>');
+    if (value.category != "")
+        list_item.addClass(value.category);
+    list_item.data("key", key);
+
+    // TOP ROW
+
+    var top_row = $('<div class="top_row"><div class="due_in">' +
+    '<div>15h</div><div class="description">due in</div></div>' +
+    '<div class="task_title"><h2>' + value.title +
+    '</h2></div></div>');
+    list_item.append(top_row);
+
+    // BOTTOM ROW
+    var bottom_row = $('<div class="bottom_row">' +
+    '<div class="difficulty"><div class="trapez trapez_1 grey"></div><div class="trapez trapez_2 grey"></div><div class="trapez trapez_3"></div><div class="trapez trapez_4"></div><div class="trapez trapez_5"></div>' +
+    '<div class="description">difficulty</div></div>' +
+    '<div class="satisfaction"><div class="laughing"></div><span class="description">satisfaction</span></div><div class="effort"><div class="invested right"><span>1</span><div class="description right">invested</div></div>' +
+    '<div class="predicted"><span>&nbsp;8m</span><div class="description">&nbsp;predicted</div></div></div>' +
+    '</div>');
+
+    list_item.append(bottom_row);
+    list_item.click(function(){ // expand / hide predictions
+        $(this).find(".bottom_row").toggle(500);
+    });
+
+    // TODO: replace dblclick function with something more meaningful
+    /* TODO: actually! add a step inbetween: color the task red, add word "remove?"
+     + some delete icon maybe, the next tap (or click) calls removeTask
+     perhaps add a timeout to this - if not deleted within 5s, revert back to normal display?
+
+     OR: swipe event gives you these options:
+     1. mark as DONE, 2. edit task, 3. delete, 4. go back
+     */
+    list_item.mousedown(function(e){
+        if( e.button == 2 ) {
+            alert('Right mouse button!');
+            removeTask(key);
+            return false;
+        }
+        return true;
+    });
+
+    list_item.dblclick(function(){
+        updateTask(key, "title", "Huhu.");
+    });
+
+    return list_item;
+}
+
+function updateTaskListView(store) {
     console.log("display task list");
     if (typeof store == 'undefined') {
         console.log("store undefined");
@@ -198,30 +316,16 @@ function displayTaskList(store) {
 //TODO display list content, or... write a display task function
         // If the cursor is pointing at something, ask for the data
         if (cursor) {
-            console.log("displayTaskList cursor:", cursor);
+            console.log("updateTaskListView cursor:", cursor);
             req = store.get(cursor.key);
             req.onsuccess = function (evt) {
+                var key = cursor.key;
                 var value = evt.target.result;
-                var list_item = $('<li class="task"></li>');
-                if (value.category != "")
-                    list_item.addClass(value.category);
-                list_item.data("key", cursor.key);
 
-                // TOP ROW
+                // TODO
+                var list_item = getTaskListElement(value, key);
 
-                var top_row = $('<div class="top_row"><div class="due_in">' +
-                '<div>15h</div><div class="description">due in</div></div>' +
-                '<div class="task_title"><h2>' + value.title +
-                '</h2></div></div>');
-                list_item.append(top_row);
 
-                // BOTTOM ROW
-                var bottom_row = $('<div class="bottom_row"><div class="difficulty"><div class="trapez trapez_1 grey"></div><div class="trapez trapez_2 grey"></div><div class="trapez trapez_3"></div><div class="trapez trapez_4"></div><div class="trapez trapez_5"></div><div class="description">difficulty</div></div><div class="satisfaction"><div class="laughing"></div><span class="description">satisfaction</span></div><div class="effort"><div class="invested right"><span>1</span><div class="description right">invested</div></div><div class="predicted"><span>8m</span><div class="description">predicted</div></div></div></div>');
-
-                list_item.append(bottom_row);
-                list_item.click(function(){ // expand / hide predictions
-                    $(this).find(".bottom_row").toggle(500);
-                });
 
 
                 ul_tasks.append(list_item);
